44d73be5c7d72b78f89f225cca79203906933159
Pavana Manikanta Reddy <manipvn@gmail.com>
2019-10-21 10:22:46 +0530

    Added the environment variables

--- run_slack.py
+++ run_slack.py
@@ -3,13 +3,13 @@ from rasa_core.agent import Agent
 from rasa_core.interpreter import RasaNLUInterpreter
 from slack.rasa_slack_connector import SlackInput
 
-
+import os
 nlu_interpreter = RasaNLUInterpreter('./models/nlu/default/restaurant_finder')
 agent = Agent.load('./models/dialogue', interpreter = nlu_interpreter)
 
-input_channel = SlackInput('xoxp-802123317696-789341201010-789601485490-423f8c5e493b38e1a9700801d889a768', #app verification token
-							'xoxb-802123317696-801919589748-zM2txMXdNETnkHDw6mJkVAdf', # bot verification token
-							'QBVtotROW4GlL9RE5hwPU6sR', # slack verification token
+input_channel = SlackInput(os.environ['OAuth_Access_Token'],#app verification token
+							os.environ['Bot_User_OAuth_Access_Token'],# bot verification token
+							os.environ['verification_token'], # slack verification token
 							True)
 
 agent.handle_channel(HttpInputChannel(5004, '/', input_channel))
